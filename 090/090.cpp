e5d9e5d3dea9690df537a986caa951ba007586ea
tranuyn <127413543+tranuyn@users.noreply.github.com>
2023-04-16 00:22:23 +0700

    ehe

---
+++ 090/090.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+bool KiemTraDoiXung(int k);
+int TongDoiXung(float[], int);
+
+int main()
+{
+	float a[1000];
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	cout << "Nhap mang a: " << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+	int s = TongDoiXung(a, n);
+	cout << " Tong cac phan tu la so doi xung trong mang la: " << s;
+	return 1;
+}
+
+bool KiemTraDoiXung(int k)
+{
+	int dao = 0, k2 = k;
+	while (k2 != 0)
+	{
+		dao = (dao * 10) + (k2%10);
+		k2 /= 10;
+	}
+	if (dao == k) return true;
+	return false;
+}
+
+int TongDoiXung(float a[], int n)
+{
+	if (n == 0)
+		return 0;
+	int s = TongDoiXung(a, n-1);
+	if (KiemTraDoiXung(a[n - 1]))
+		s += a[n - 1];
+	return s;
+}
